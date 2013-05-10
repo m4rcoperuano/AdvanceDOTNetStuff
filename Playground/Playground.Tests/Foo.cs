34f714f8555450737700b27f19fa680855115e6d
Marco Ledesma <marco.j.ledesma@gmail.com>
2013-05-09 23:51:51 -0400

    Starting up some tests

---
+++ Playground/Playground.Tests/Foo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Playground.Classes;
+using Playground.Interfaces;
+
+namespace Playground.Tests
+{
+    [TestClass]
+    public class CarStuff
+    {
+        [TestMethod]
+        public void Operations()
+        {
+            //a car has a speed property
+            Car car = new Car();
+            car.Speed = 50;
+            //need to retrieve it
+            double modifiedSpeed = this.GetTheModifiedSpeed(car);
+            Assert.AreEqual(car.Speed + 70, modifiedSpeed);
+        }
+
+        [TestMethod]
+        public void GenerateCSV()
+        {
+            //take a class full of properties
+            MyCSVClass csvClass = new MyCSVClass();
+            List<string> values = new List<string>();
+            //break it into a dictionary object
+            csvClass.Name = "hello World";
+            csvClass.Address = "stuff 123";
+            csvClass.ZipCode = "22222";
+
+            var properties = csvClass.GetType().GetProperties().ToDictionary(x => x.Name, x => x.GetValue(csvClass, null));
+            foreach (var prop in properties)
+            {
+                values.Add(prop.Value.ToString());
+            }
+            Assert.AreEqual(values.Count, 3);
+        }
+
+        //helper methods
+        public double GetTheModifiedSpeed(IVehicles vehicle)
+        {
+            return vehicle.GetSpeed() + 70;
+        }
+    }
+}
---
+++ Playground/Playground/Classes/Car.cs
@@ -0,0 +1,18 @@
+﻿using Playground.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Playground.Classes
+{
+    public class Car : IVehicles
+    {
+        public double GetSpeed()
+        {
+            return this.Speed;
+        }
+
+        public double Speed { get; set; }
+    }
+}
